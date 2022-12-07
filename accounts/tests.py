a46943faf3760dd4d559518e8cab6247a4eb175c
klimrus61 <klimrus61@mail.ru>
2022-12-07 15:05:57 +0300

    Зеленые тесты представления валидации пользователя

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,4 +1,9 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+
+class UserModelTest(TestCase):
+    '''Тест модели пользователя'''
 
 
 class AccountsViewTest(TestCase):
@@ -8,3 +13,14 @@ class AccountsViewTest(TestCase):
         """тест: используется шаблон login"""
         response = self.client.get('/accounts/login/')
         self.assertTemplateUsed(response, 'login.html')
+
+    def test_validate_data_from_form_login(self):
+        """тест: валидация данных из отправленной формы"""
+        user = User.objects.create_user(username='test', password='test')
+
+        response = self.client.post('/accounts/login/', {'username': 'test', 'password': 'test'})
+        self.assertEqual(response.status_code, 200)
+
+        response = response = self.client.post('/accounts/login/', {'username': 'wrong_user', 'password': 'wrong_password'})
+        self.assertNotEqual(response.status_code, 200)
+        print(response.status_code)
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
 
 
 def login_page(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'home.html')
+        return redirect(reverse('login_page'))
     return render(request, "login.html")
